852f60c97f95bcb4dcf8fa38717cd3c9ab4892c6
EnmaLlDev <enmanuel.lledo.1998@gmail.com>
2025-05-27 09:01:47 +0200

    Resuelto!

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,30 @@
 package com.campusdual.classroom;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
+        String ruta = "src/main/resources/lorem.txt";
+        File fichero = new File(ruta);
 
+        try{
+            if (fichero.exists()) {
+                FileReader fr = new FileReader(fichero);
+                BufferedReader br = new BufferedReader(fr);
+                String linea;
+
+                while ((linea = br.readLine()) != null) {
+                    System.out.println(linea);
+                }
+            }
+            else {
+                System.out.println("El fichero no existe.");
+            }
+        } catch (Exception e) {
+            System.err.println("Error al acceder al fichero: " + e.getMessage());
+        }
     }
 }
